06eb1206bf1afdfc89e738a97808f77ce5d423f6
닮 <dhdpekfa1@daum.net>
2025-02-12 11:16:30 +0900

    [design] 입력 화면 디자인 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,6 +15,7 @@ import {
   Textarea,
 } from '@/components/ui';
 import { toast } from '@/hooks/use-toast';
+import { Layout } from '@/components/frame';
 
 const HomePage = () => {
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -36,8 +37,8 @@ const HomePage = () => {
     toast({
       title: '작성하신 정보를 바탕으로 분석을 시작합니다.',
       description: (
-        <pre className='mt-2 w-[340px] rounded-md bg-deep-green p-4'>
-          <code className='text-white'>
+        <pre className='mt-2 w-[340px] rounded-t-md bg-blue p-4'>
+          <code className='text-'>
             최종 목표: {data.goal}
             {data.description && `\n설명: ${data.description}`}
             {data.subjects.length > 0 &&
@@ -49,118 +50,120 @@ const HomePage = () => {
   };
 
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-50'>
-      <div className='w-full flex flex-col items-center justify-center text-base md:text-xl lg:text-2xl'>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='w-2/3 space-y-6'
-          >
-            {/* Goal 필드 */}
-            <FormField
-              control={form.control}
-              name='goal'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className='text-sm md:text-xl lg:text-2xl'>
-                    최종 목표
+    <Layout>
+      <div className='flex flex-col justify-center bg-two'>
+        <div className='flex flex-col items-center justify-center text-base md:text-xl'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
+              {/* Goal 필드 */}
+              <div className='bg-two -t-md p-6 text-center'>
+                <FormField
+                  control={form.control}
+                  name='goal'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className='text-sm md:text-xl text-ef'>
+                        최종 목표
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className='text-xs md:text-base w-full md:w-1/2 lg:w-1/3 mx-auto text-center'
+                          placeholder='최종 목표를 입력해주세요.'
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription className='ml-3 text-xs md:text-base text-neutral-400'>
+                        * 최종 목표를 기반으로 분석합니다.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className='bg-[#f2f6f7] rounded-t-xl p-6 flex flex-col gap-8'>
+                {/* 동적 과목 필드 */}
+                <div className='flex flex-col gap-2'>
+                  <FormLabel className='text-sm md:text-xl text-three'>
+                    학습 중인 과목
                   </FormLabel>
-                  <FormControl>
-                    <Input
-                      className='text-xs md:text-base lg:text-lg w-full'
-                      placeholder='최종 목표를 입력해주세요.'
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription className='ml-3 text-xs md:text-base lg:text-lg text-gray-500'>
-                    * 최종 목표를 기반으로 분석합니다.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {/* Description 필드 */}
-            <FormField
-              control={form.control}
-              name='description'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className='text-sm md:text-xl lg:text-2xl'>
-                    추가 설명 <span className='text-gray-400'>[선택]</span>
-                  </FormLabel>
-                  <FormControl>
-                    <Textarea
-                      className='text-xs md:text-base lg:text-lg'
-                      placeholder='현재 준비하고 계신 부분이나 학습 중인 부분을 자세하게 작성해주시면 분석에 도움됩니다. '
-                      {...field}
-                    />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {/* 동적 과목 필드 */}
-            <div>
-              <FormLabel className='text-sm md:text-xl lg:text-2xl'>
-                학습 중인 과목
-              </FormLabel>
-              {fields.map((field, index) => (
-                <div
-                  key={field.id}
-                  className='flex items-center space-x-2 mt-2'
-                >
-                  <FormField
-                    control={form.control}
-                    name={`subjects.${index}.value`}
-                    render={({ field }) => (
-                      <FormItem className='flex-1'>
-                        <FormControl>
-                          <Input
-                            className='text-xs md:text-base lg:text-lg'
-                            placeholder='학습중인 과목을 입력해주세요.'
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  {fields.length > 1 && (
+                  {fields.map((field, index) => (
+                    <div key={field.id} className='flex items-center space-x-2'>
+                      <FormField
+                        control={form.control}
+                        name={`subjects.${index}.value`}
+                        render={({ field }) => (
+                          <FormItem className='flex-1'>
+                            <FormControl>
+                              <Input
+                                className='text-xs md:text-base'
+                                placeholder='학습중인 과목을 입력해주세요.'
+                                {...field}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      {fields.length > 1 && (
+                        <Button
+                          type='button'
+                          className='bg-red-500/40 hover:bg-red-400 h-fit py-1 px-2 text-xs md:text-base'
+                          onClick={() => remove(index)}
+                        >
+                          삭제
+                        </Button>
+                      )}
+                    </div>
+                  ))}
+                  <div className='w-full flex items-center justify-center'>
                     <Button
                       type='button'
-                      className='bg-red-500 hover:bg-red-400 h-fit py-1 px-2 text-xs md:text-base lg:text-lg'
-                      onClick={() => remove(index)}
+                      onClick={() => append({ value: '' })}
+                      className='h-fit py-1 px-2 md:px-3 text-xs md:text-sm text-four bg-blue hover:bg-green hover:text-four my-2'
                     >
-                      삭제
+                      과목 추가
                     </Button>
-                  )}
+                  </div>
                 </div>
-              ))}
-              <div className='w-full flex items-center justify-center'>
+
+                {/* Description 필드 */}
+                <FormField
+                  control={form.control}
+                  name='description'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className='text-sm md:text-xl text-three'>
+                        추가 설명{' '}
+                        <span className='text-neutral-400'>[선택]</span>
+                      </FormLabel>
+                      <FormControl>
+                        <Textarea
+                          className='text-xs md:text-base'
+                          placeholder='현재 준비하고 계신 부분이나 학습 중인 부분을 자세하게 작성해주시면 분석에 도움됩니다. '
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className='flex items-center justify-center bg-[#f2f6f7]'>
                 <Button
-                  type='button'
-                  onClick={() => append({ value: '' })}
-                  className='h-fit py-1 px-2 text-xs md:text-base lg:text-lg text-deep-green bg-white border border-deep-green hover:bg-deep-green hover:text-white my-2'
+                  className='bg-blue hover:bg-green w-2/3 md:w-1/2 lg:w-1/3 text-sm md:text-base text-four'
+                  type='submit'
                 >
-                  추가
+                  제출
                 </Button>
               </div>
-            </div>
-            <div className='w-full flex items-center justify-center'>
-              <Button
-                className='bg-deep-green hover:bg-green w-full md:w-1/2 lg:w-1/3 text-sm md:text-base lg:text-lg'
-                type='submit'
-              >
-                제출
-              </Button>
-            </div>
-          </form>
-        </Form>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
